844fcb1fe24a3adc19b4c6b140f9d6889fe7f225
dharma mudita <dharmamudita404@gmail.com>
2026-07-29 03:48:32 +0700

    feat: update auth.routes.js

--- packages/api/routes/auth.routes.js
+++ packages/api/routes/auth.routes.js
@@ -9,5 +9,7 @@ router.post('/login', authController.login);
 
 // Protected routes (Butuh Token)
 router.get('/me', verifyToken, authController.getMe);
+router.put('/profile', verifyToken, authController.updateProfile);
+router.put('/change-password', verifyToken, authController.changePassword);
 
 module.exports = router;
